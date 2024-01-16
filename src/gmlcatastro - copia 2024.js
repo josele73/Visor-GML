a0ee35239d161cc384860fca02233b6819639a2d
josele73 <44363614+josele73@users.noreply.github.com>
2024-01-16 14:35:45 +0100

    Add files via upload

---
+++ src/gmlcatastro - copia 2024.js
@@ -0,0 +1,294 @@
+// Extraer datos GML
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+
+function extraeUso(lineas){
+    for(var i = 0; i < lineas.length; i++)
+        if (lineas[i].indexOf('25831') != -1)
+            return true;
+
+}
+
+
+function extraeTag(lineas){   
+    var lineaPuntos=new String ;
+    var lineaId;
+    var lineaNamespace;
+    var lineaAreavalue;
+    var lineaLabel;
+    var cuerpos=0;
+    var cuerposF=0;
+    var listadoCuerpos=[];
+    var listadoFinCuerpos=[];
+    var ArrayListadoPuntos=[];
+    var ArrayId=[];
+    var contId=0;
+    var ArrayNamespace=[];
+    var contNamespace=0;
+    var ArrayArea=[];
+    var contArea=0;
+    var lineaMuchosPuntos;
+    var usoN=0;
+    var esConstruccion=0;
+    var tributa=0;
+
+    
+    for(var i = 0; i < lineas.length; i++)
+        {
+        if (lineas[i].indexOf('localId') != -1) {
+            ArrayId[contId]=lineas[i];
+            ArrayId[contId]=extraerDatos(ArrayId[contId]);
+            contId=contId+1;
+            } //localID
+        
+        if (lineas[i].indexOf('namespace') != -1) {
+            ArrayNamespace[contNamespace]=lineas[i];
+            ArrayNamespace[contNamespace]=extraerDatos(ArrayNamespace[contNamespace]);
+            contNamespace=contNamespace+1;
+            } //namespace
+        
+        if (lineas[i].indexOf('areaValue') != -1) {
+            ArrayArea[contArea]=lineas[i];
+            ArrayArea[contArea]=extraerDatos(ArrayArea[contArea]);
+            contArea=contArea+1;
+            } //namespace
+
+        if (lineas[i].indexOf('<gml:posList') != -1)
+            { 
+                listadoCuerpos[cuerpos]=i;
+                cuerpos=cuerpos+1;
+            } //postlist
+            
+        if (lineas[i].indexOf('</gml:posList') != -1)
+            { 
+                listadoFinCuerpos[cuerposF]=i;
+                cuerposF=cuerposF+1;
+                
+            } // /postlist
+
+        if (lineas[i].indexOf('25831') != -1)
+            { 
+               usoN=1;
+               //console.log("31N")
+            } //uso 31
+
+        if (lineas[i].indexOf('ES.SDGC.BU') != -1)
+            { 
+               esConstruccion=1;
+               //console.log("Es construccion")
+            } //es construccion
+    
+            if (lineas[i].indexOf('ES.SDGC.CP') != -1)
+            { 
+               tributa=1;
+               //console.log("Esta tributando")
+            } //es construccion
+
+        }//for var i
+        
+        /*
+        console.log(ArrayId);
+        console.log(ArrayNamespace);
+        console.log(ArrayArea);
+        console.log("CUERPOS");
+        console.log(listadoCuerpos);
+        console.log(listadoFinCuerpos);
+        console.log(cuerpos);
+        
+        if (cuerpos==1)  //vamos a preparar las lineas de los puntos en una sola
+            {
+             if (listadoCuerpos[0]==listadoFinCuerpos[0])
+                console.log("Puntos en misma linea");
+             else
+                console.log("PUNTOS EN VARIAS LINEAS")   
+            }
+        else
+            {
+            console.log("VARIOS CUERPOS");
+            }//else
+        
+        */
+
+        for(var z = 0; z < listadoCuerpos.length; z++)  //Cargamos lass lineas con los puntos
+            {
+            lineaMuchosPuntos="";
+            if (listadoCuerpos[z]<listadoFinCuerpos[z])
+                {
+                //console.log(listadoCuerpos[z],listadoFinCuerpos[z]);
+                for(var l = listadoCuerpos[z]; l <= listadoFinCuerpos[z]; l++)
+                    {
+                    //console.log(lineas[l]);
+                    lineaMuchosPuntos+=lineas[l]+" ";
+                    }
+                }
+            if (listadoCuerpos[z]=listadoFinCuerpos[z]) // si esta todo en la misma linea
+                lineaMuchosPuntos+=lineas[listadoCuerpos[z]];
+                
+            ArrayListadoPuntos[z]=lineaMuchosPuntos;
+            }
+
+        //console.log(ArrayListadoPuntos[0]);
+
+        for (var z = 0; z < ArrayListadoPuntos.length; z++){ //Array de varios cuerpos
+            prueba=extraerDatos(ArrayListadoPuntos[z]);
+            listado=extraerPuntos(prueba);
+            //console.log(ArrayListadoPuntos[0]);
+            //console.log("listado");
+            //console.log(listado);
+            puntosJ=puntosJSON(listado);
+            //console.log(puntosJ)
+            crearJSON("-1","-1","-1",puntosJ,usoN);
+            } //for
+            
+            //construccion tabla contenido
+            //cargar foto
+            //console.log(ArrayId)
+            var total=""
+            var foto=" "
+            if (tributa==1)
+                {
+               
+                foto = '<ul><img src="http://ovc.catastro.meh.es/OVCServWeb/OVCWcfLibres/OVCFotoFachada.svc/RecuperarFotoFachadaGet?ReferenciaCatastral=' + ArrayId[0] + '" width="300" height="200"></img></ul>';
+                total  += '<table width="300"><caption><b><center>Datos Fichero GML</center></b></caption><tr><td>TIPO: </td><td> Parcela Catastral</td></tr>'
+                total += '<tr><td>ID: </td><td>'+ArrayId[0]+'</td></tr>'
+                total  += '<tr><td>NameSpace: </td><td>'+ArrayNamespace[0]+'</td></tr>'
+                if (usoN==true)
+                    total  += '<tr><td>UTM: </td><td>UTM 31N ETRS89</td></tr>'
+                else
+                    total  += '<tr><td>UTM: </td><td>UTM 30N ETRS89</td></tr>'
+                total += '<tr><td>Superficie: </td><td>'+ArrayArea[0]+' m2</td></tr></table>'
+                 }
+            else{
+                foto=" ";
+                total  += '<table width="300"><caption><b><center>Datos Fichero GML</center></b></caption><tr><td>TIPO: </td><td> Edificio - Construcción</td></tr>'
+                total += '<tr><td>ID: </td><td>'+ArrayId[0]+'</td></tr>'
+                total  += '<tr><td>NameSpace: </td><td>'+ArrayNamespace[0]+'</td></tr>'
+                if (usoN==true)
+                    total  += '<tr><td>UTM: </td><td>UTM 31N ETRS89</td></tr>'
+                else
+                    total  += '<tr><td>UTM: </td><td>UTM 30N ETRS89</td></tr>'
+                total += '<tr><td>Total cuerpos: </td><td>'+cuerposF+'</td></tr></table>'
+                
+                }
+                total+='<center><input name="btnImprimir" id="btnImprimir" type="button" class="button" value="Imprimir" onClick="imprSelec()"></center>'
+                document.getElementById('list').innerHTML +=total
+                document.getElementById('foto').innerHTML =foto
+            //console.log(total);
+            //console.log(document.getElementById('list'));
+            //console.log(tributa);    
+
+        
+} //fin extraerTAG
+
+function extraerDatos(li)  //extraemos posiciones de <> <>
+    {
+    //console.log("Extarer datos");
+    //console.log(li); 
+    var inicioFinal; 
+    var posiciones=[];
+    var pos=0;
+    for(var i = 0; i < li.length-1; i++){
+        if (li[i].indexOf('>') != -1)
+            {//console.log(i);
+            posiciones[pos]=i;
+            pos+=1;}
+        if (li[i].indexOf('<') != -1)
+            {//console.log(i);
+            posiciones[pos]=i;
+        pos+=1;}
+        }
+    //devolvemos cadena entre > <
+    return (li.substr(posiciones[1]+1,posiciones[2]-posiciones[1]-1));
+}
+
+
+function extraerPuntos(p){  //se llama desde extraerTAG
+    //console.log("extraerpuntos");
+    //console.log(p);  
+    p=p.trim();
+    
+    puntos=p.split(" ");
+    //console.log(puntos.length);
+    //console.log(puntos);
+    totalPuntos=(puntos.length/2);
+    //console.log(totalPuntos);
+    //console.log(puntos[0],puntos[1]);
+     //quitamos si hay puntos vacios por varios blancos despues del splir
+     puntitosOk=[]
+     k=0;
+    for (x=0;x<puntos.length;x++){  //Aqui quitamos los espacios en blanco cuando hay muchos
+        if (puntos[x]!=""){
+            puntitosOk[k]=puntos[x];
+            k=k+1;}
+            //puntos.splice(x, 1);
+            
+    }
+    //console.log(puntos[2],puntos[3]);
+   
+    //console.log(puntitosOk);
+    puntos=puntitosOk;
+    totalPuntos=(puntos.length/2);
+    
+    //console.log(totalPuntos);
+    var nuevosPuntos=[];
+    for (x=0;x<totalPuntos;x++){
+        //console.log(puntos[x*2]+","+puntos[(x*2)+1]);
+        //console.log(convertir(puntos[x*2],puntos[(x*2)+1]));	
+        nuevosPuntos+=(convertir(puntos[x*2],puntos[(x*2)+1]))+",";
+        //nuevosPuntos.push(convertir(puntos[x*2]));
+        //nuevosPuntos.push(convertir(puntos[(x*2)+1]));
+        }
+    //console.log("nuevosPuntos");
+    //console.log(nuevosPuntos);
+    return nuevosPuntos;
+}
+
+
+function puntosJSON(lista){  //se llama desde extraer TAG
+    //console.log(lista);
+    lista=lista.split(",")
+    //lista[0]=lista[0].substr(9,lista[0].length) //quitamos el undefined ?¿?¿?
+    //console.log(lista);
+    var list;
+    for (x=0;(x<(lista.length-3)/2);x++){
+        //console.log(lista[x*2]+","+lista[(x*2)+1]);	
+        list+=("[ "+lista[x*2]+" , "+lista[(x*2)+1]+" ],");
+        }
+    list+=("["+lista[(lista.length-3)]+","+lista[(lista.length-2)]+"]");
+    list=list.substr(9,list.length); //quitamos el undefined ?¿?¿?
+    //console.log(list);
+    return list;
+}
+
+
+function crearJSON(iden,nameSp,superficie,puntosWS84){ 
+    //console.log(puntosWS84)
+    jsonJ  = '{ "type": "Feature", "properties": { "gml_id": "'+iden+'", "areaValue":'+superficie+', "areaValue_uom": "m2", "localId": "'+iden+'", "namespace": "'+nameSp+'","nationalCadastralReference": "'+iden+'" },"geometry": { "type": "MultiPolygon", "coordinates": [[['+puntosWS84+']]] } }';
+    //console.log(jsonJ);
+    //transformamos el texto a JSON
+    var jsonJOK = JSON.parse(jsonJ);
+    //sacamos en mapa;
+    L.geoJSON(jsonJOK,{style: myStyle}).addTo(map);
+    //zoom al json cargado
+    map.fitBounds(L.geoJSON(jsonJOK).getBounds());
+   
+}
+
+
+
+
+function imprSelec() {
+    
+    var div1 = document.getElementById('side-bar');
+    div1.style.display = 'none'
+    window.print();
+    div1.style.display = 'block'
+    
+
+}
+
+  
--- src/gmlcatastro - este va bien de uno en uno.js
+++ src/gmlcatastro - este va bien de uno en uno.js
@@ -1,19 +1,9 @@
-//funciona con visor 3.0
-//carga varios gml al arrastraslos a la vez
-//carga varios gml de forma secuencial
-//añadida ortofoto icv 2023
-
-// Estilo para el mapa usando Leaflet para mostrar el mapa
-
+// Estilo para el mapa (asumiendo que estás usando Leaflet para mostrar el mapa)
 var myStyle = {
     "color": "#ff7800",
     "weight": 5,
     "opacity": 0.65
 };
-
-// Registro de capas GML añadidas al mapa
-
-var capasGML = [];
 
 function extraeUso(lineas) {
     for (var i = 0; i < lineas.length; i++) {
@@ -25,7 +15,6 @@ function extraeUso(lineas) {
 }
 
 // Funciones Auxiliares
-
 function extraerContenidoDeTag(tag) {
     const regex = />(.*?)</;
     const match = regex.exec(tag);
@@ -33,33 +22,47 @@ function extraerContenidoDeTag(tag) {
 }
 
 function extraerPuntosDePosList(posListText) {
-    //console.log("posListText original:", posListText); // Mostrar el texto original en la consola
+    console.log("posListText original:", posListText); // Mostrar el texto original en la consola
+
+    // Eliminar etiquetas XML y espacios en blanco al inicio y al final
     const textoLimpio = posListText.replace(/<[^>]*>/g, '').trim();
-    //console.log("Texto limpio:", textoLimpio); // Mostrar el texto limpio en la consola
+    console.log("Texto limpio:", textoLimpio); // Mostrar el texto limpio en la consola
+
+    // Reemplazar múltiples espacios en blanco (incluidos tabuladores y saltos de línea) con un solo espacio
     const textoNormalizado = textoLimpio.replace(/\s+/g, ' ');
-    //console.log("Texto normalizado:", textoNormalizado); // Mostrar el texto normalizado
+    console.log("Texto normalizado:", textoNormalizado); // Mostrar el texto normalizado
+
+    // Dividir la cadena en puntos X e Y
     const puntos = textoNormalizado.split(' ');
-    //console.log("Puntos divididos:", puntos); // Mostrar los puntos divididos
+    console.log("Puntos divididos:", puntos); // Mostrar los puntos divididos
+
     const puntosX = [];
     const puntosY = [];
+
     for (let i = 0; i < puntos.length; i += 2) {
         puntosX.push(parseFloat(puntos[i]));
         puntosY.push(parseFloat(puntos[i + 1]));
     }
-    return { puntosX, puntosY }; // Asegurarse de devolver un objeto
+
+    return { puntosX, puntosY };
 }
 
 function convertir(puntoX, puntoY) {
-    return [puntoX, puntoY]; // Función para convertir puntos, si es necesario
+    // Función para convertir puntos, si es necesario
+    return [puntoX, puntoY];
 }
 
-function crearJSON(iden, nameSp, superficie, puntosWS84){ //, usoN) {
+function crearJSON(iden, nameSp, superficie, puntosWS84, usoN) {
     var jsonJ = '{ "type": "Feature", "properties": { "gml_id": "' + iden + '", "areaValue":' + superficie + ', "areaValue_uom": "m2", "localId": "' + iden + '", "namespace": "' + nameSp + '","nationalCadastralReference": "' + iden + '" },"geometry": { "type": "MultiPolygon", "coordinates": [[[' + puntosWS84 + ']]] } }';
+    
+    // Transformamos el texto a JSON
     var jsonJOK = JSON.parse(jsonJ);
-    var nuevaCapa = L.geoJSON(jsonJOK, {style: myStyle});
-    nuevaCapa.addTo(map);
-    capasGML.push(nuevaCapa); // Añadir la nueva capa al registro
-    map.fitBounds(nuevaCapa.getBounds());
+
+    // Sacamos en mapa;
+    L.geoJSON(jsonJOK, {style: myStyle}).addTo(map);
+
+    // Zoom al json cargado
+    map.fitBounds(L.geoJSON(jsonJOK).getBounds());
 }
 
 function puntosJSON(lista) {
@@ -68,7 +71,6 @@ function puntosJSON(lista) {
 }
 
 // Función Principal para Extraer Datos de las Etiquetas
-
 function extraeTag(lineas) {
     const ArrayId = [];
     const ArrayNamespace = [];
@@ -76,7 +78,10 @@ function extraeTag(lineas) {
     let esConstruccion = false;
     let tributa = false;
     let usoN = false;
+
+    // Unir todas las líneas en una sola cadena de texto
     const textoCompleto = lineas.join('\n');
+    
     lineas.forEach(linea => {
         if (linea.includes('localId')) {
             ArrayId.push(extraerContenidoDeTag(linea));
@@ -92,16 +97,37 @@ function extraeTag(lineas) {
             tributa = true;
         }
     });
+
+    // Modificar la expresión regular para manejar etiquetas en la misma línea
     const posListRegex = /<gml:posList[^>]*>([\s\S]*?)<\/gml:posList>/g;
     let match;
+	
+	//var total = '<center><input name="btnImprimir" id="btnImprimir" type="button" class="button" value="Imprimir" onClick="imprSelec()"></center>';
+    //document.getElementById('list').innerHTML += total;
+    //document.getElementById('foto').innerHTML = foto;
+	
+	// Crear una cadena HTML para el botón de imprimir
+    var total = '<center><input name="btnImprimir" id="btnImprimir" type="button" class="button" value="Imprimir" onClick="imprSelec()"></center>';
+
+    // Agregar el botón de imprimir a la barra lateral
+    var barraLateral = document.getElementById('side-bar');
+    if (barraLateral) {
+        barraLateral.innerHTML += total;
+    } else {
+        console.error("No se encontró el elemento 'side-bar'");
+    }
+
     while ((match = posListRegex.exec(textoCompleto)) !== null) {
         const posListText = match[1];
+        console.log("Encontrado posListText:", posListText); // Agregar para ver el texto encontrado
         const { puntosX, puntosY } = extraerPuntosDePosList(posListText);
+        
         const puntosConvertidos = puntosX.map((x, i) => convertir(x, puntosY[i]));
         const puntosEnJSON = puntosJSON(puntosConvertidos);
         crearJSON("-1", "-1", "-1", puntosEnJSON, usoN);
     }
 }
+
 
 // Función para imprimir la selección
 function imprSelec() {
@@ -109,4 +135,13 @@ function imprSelec() {
     div1.style.display = 'none';
     window.print();
     div1.style.display = 'block';
+	document.getElementById('list').innerHTML += total;
+	document.getElementById('foto').innerHTML = foto;
 }
+
+
+
+
+// CSS
+// .hide-for-print { display: none !important; }
+
